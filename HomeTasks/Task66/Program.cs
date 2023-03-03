10677045cc6f0e8ce1f58ba3c56372fd14ac5026
Nikolay Borovik <nik.Borovik@bk.ru>
2023-03-03 19:27:37 +1000

    Task66 done

--- HomeTasks/Task66/Program.cs
+++ HomeTasks/Task66/Program.cs
@@ -3,3 +3,23 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+Console.Clear();
+
+Random rnd = new Random();
+int n = rnd.Next(1, 20);
+int m = rnd.Next(1, 10);
+System.Console.WriteLine($"{n},{m}");
+
+int SumOfIntNum (int n, int m)
+{
+    int count = 0;
+    for (int i = m; i <= n; i++)
+    {
+        count += i;
+    }
+    return count;
+}
+
+int result = SumOfIntNum(n, m);
+
+System.Console.WriteLine (result);
